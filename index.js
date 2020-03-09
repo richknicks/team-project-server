3ba994c05aee70d8f5bed27013ac0adc8acf8169
richknicks <rlynch@gmail.com>
2020-03-09 19:49:58 -0400

    added books array

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ const server = express();
 server.use(express.json());
 const PORT = 5432;
 
+let books =[{
+    id:1,
+    title:"Becoming a Devoloper",
+    year:2020
+},
+{
+    id:2,
+    title:"A book",
+    year:2021
+},
+{
+    id:3,
+    title:"Some book",
+    year:2019
+}]
+
+server.get('/api/books', (req,res)=>{
+    res.status(200).json(books)
+})
+
 server.listen(PORT, () => console.log(`\n ** API on http://localhost:${PORT} **\n)`));
